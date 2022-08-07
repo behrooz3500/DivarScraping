a5a2cb25967fec02ee5b8c4d3e12f0109cd29a79
behrooz3500 <behrooz3500@yahoo.com>
2022-08-07 20:29:40 +0430

    add a load history to the widget

--- src/widgets/subwidgets/Url_Widget.py
+++ src/widgets/subwidgets/Url_Widget.py
@@ -5,7 +5,7 @@ from urllib.parse import unquote
 from src.Constants import *
 
 # PyQt5
-from PyQt5.QtWidgets import QLineEdit, QWidget
+from PyQt5.QtWidgets import QLineEdit, QWidget, QComboBox
 from PyQt5.QtCore import Qt
 
 
@@ -16,10 +16,19 @@ class UrlWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.lb = QLineEdit()
-        self.lb.setPlaceholderText(URL_HINT_TEXT)
-        self.lb.setAlignment(Qt.AlignLeft)
-        self.lb.setText(unquote(DEFAULT_URL))
+        self.edit_combo = QComboBox()
+        self.edit_combo.setEditable(True)
+        self.edit_combo.setPlaceholderText(URL_HINT_TEXT)
+        self.edit_combo.addItems(self.load_history())
 
+    def load_history(self):
+        history_set = set()
+        try:
+            with open('history.txt', 'rt', encoding="utf-8") as f:
+                for line in f:
+                    strip_lines = line.strip()
+                    history_set.add(strip_lines)
+        except:
+            pass
 
-
+        return history_set
