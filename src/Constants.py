2bf29acce0fb60715b42c28c0809e5feae842749
behrooz3500 <behrooz3500@yahoo.com>
2022-08-07 12:40:40 +0430

    define a module to store constants

---
+++ src/Constants.py
@@ -0,0 +1,31 @@
+# MainWindow Constants
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 400
+WINDOWS_X = 250
+WINDOWS_Y = 250
+WINDOWS_TITLE = "Divar Scraping"
+FILL_THE_BLANKS_ERROR = "PLEASE FILL IN THE BLANKS"
+OUTPUT_TEXT_FILE_NAME = "links.txt"
+EXECUTE_FIRST_ERROR = "Please Execute the program first"
+
+# Url_Widget Constants
+DEFAULT_URL = 'https://divar.ir/s/iran/home-kitchen?q=%D9%84%D9%88%D8%A7%D8%B2%D9%85%20%D8%AE%D8%A7%D9%86%DA%AF%DB%8C'
+URL_HINT_TEXT = "Enter URL please..."
+
+# Label_Widget Constants
+SCROLL_COUNT_LABEL_TEXT = "Number of Scrolls:"
+DELAY_LABEL_TEXT = "Delay in seconds:"
+PATTERN_LABEL_TEXT = "Url pattern:"
+PATTERN_BOX_DEFAULT = 'https://divar.ir/v'
+SCROLL_INPUT_WIDTH = 40
+DELAY_INPUT_WIDTH = 25
+PATTERN_INPUT_WIDTH = 150
+INPUT_HEIGHT = 20
+
+# Result_Widget Constants
+LINKS_BOX_WIDTH = 580
+LINKS_BOX_HEIGHT = 290
+
+# Button_Widget Constants
+EXEC_BUTTON_TEXT = "Execute"
+EXPORT_BUTTON_TEXT = "Export"
