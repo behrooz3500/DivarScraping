fc2b2238257cadca2c72c972dd3d6352bb86cd1a
behrooz3500 <behrooz3500@yahoo.com>
2022-08-08 15:58:39 +0430

    1-create a parent message box 2-add question box child

--- src/widgets/Message_Box.py
+++ src/widgets/Message_Box.py
@@ -5,14 +5,35 @@ from PyQt5.QtWidgets import QMessageBox
 from src.Constants import *
 
 
-class MessageBox:
-    def __init__(self, message):
+class BaseMessageBox:
+    def __init__(self):
         super().__init__()
         self.title = BOX_TITLE
-        self.message = message
+        self.message = ""
 
     def pop_up_box(self):
         message = QMessageBox()
         message.setWindowTitle(self.title)
         message.setText(self.message)
         message.exec_()
+
+
+class MessageBox(BaseMessageBox):
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+
+
+class QuestionMessage(BaseMessageBox):
+    def __init__(self, parent):
+        super().__init__()
+        self.message = QMessageBox.question(parent, self.title, CLEAR_HISTORY_TEXT,
+                                            QMessageBox.Yes | QMessageBox.No, QMessageBox.No)
+
+    def close_event(self):
+        if self.message == QMessageBox.Yes:
+            return True
+        else:
+            return False
+
+
