059b2d1aa2da192c4826abf2f1551b4772cfc099
behrooz3500 <behrooz3500@yahoo.com>
2022-08-08 15:53:49 +0430

    add new constant variables

--- src/Constants.py
+++ src/Constants.py
@@ -6,18 +6,22 @@ WINDOWS_Y = 250
 WINDOWS_TITLE = "Divar Scraping"
 FILL_THE_BLANKS_ERROR = "Please fill in the blanks"
 OUTPUT_TEXT_FILE_NAME = "links.txt"
+HISTORY_TEXT_FILE_NAME = "history.txt"
 EXECUTE_FIRST_ERROR = "Please Execute the program first"
 WRONG_INPUT = "Please enter valid number"
 
 # Url_Widget Constants
 DEFAULT_URL = 'https://divar.ir/s/iran/home-kitchen?q=%D9%84%D9%88%D8%A7%D8%B2%D9%85%20%D8%AE%D8%A7%D9%86%DA%AF%DB%8C'
 URL_HINT_TEXT = "Enter URL please..."
+URL_BOX_WIDTH = 500
+CLEAR_HISTORY_BUTTON_TEXT = "Clear history"
 
 # Label_Widget Constants
 SCROLL_COUNT_LABEL_TEXT = "Number of Scrolls:"
 DELAY_LABEL_TEXT = "Delay in seconds:"
 PATTERN_LABEL_TEXT = "Url pattern:"
 PATTERN_BOX_DEFAULT = 'https://divar.ir/v'
+BROWSER_LIST = ["Firefox", "Chrome"]
 SCROLL_INPUT_WIDTH = 40
 DELAY_INPUT_WIDTH = 25
 PATTERN_INPUT_WIDTH = 150
@@ -33,6 +37,9 @@ EXPORT_BUTTON_TEXT = "Export"
 
 # MessageBox Constants
 BOX_TITLE = "Attention"
+CLEAR_HISTORY_TEXT = "Clear all histories?"
 
 # Scraper Constants
 IN_PROGRESS_MESSAGE = "Not available in current version"
+FIREFOX_32_DRIVER_DIR = "./Drivers/geckodriver32.exe"
+FIREFOX_64_DRIVER_DIR = "./Drivers/geckodriver64.exe"
