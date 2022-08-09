a8e183ebcd06a0b00eea6e3f70db95b1a203288b
Behrooz Ghorbani <behrooz3500@yahoo.com>
2022-08-09 13:58:13 +0430

    edit main structure of the class

--- src/thread.py
+++ src/thread.py
@@ -1,19 +1,25 @@
 # PyQt5
 from PyQt5.QtCore import QObject, pyqtSignal, QThread
 
+# standard
+from threading import Event
+
 
 class WorkerSignals(QObject):
     """Scraper Signals"""
-    progress = pyqtSignal(list)
-    error = pyqtSignal(object)
-    result = pyqtSignal(object)
-    done = pyqtSignal()
+    start = pyqtSignal()
+    stop = pyqtSignal()
+    pause = pyqtSignal()
+    resume = pyqtSignal()
 
 
 class ScrapeEngine(QThread):
     """Scraper Engine"""
     def __init__(self):
         super().__init__()
+        self.stopEvent = Event()
+        self.resumeEvent = Event()
+        self.signals = WorkerSignals()
 
     def start(self):
         pass
@@ -33,5 +39,5 @@ class ScrapeEngine(QThread):
     def run(self):
         pass
 
-    
+
 
