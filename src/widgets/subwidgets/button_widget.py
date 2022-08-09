e5d4ca5afac99e1d9b5e34ad7a39ae03f84d6e86
Behrooz Ghorbani <behrooz3500@yahoo.com>
2022-08-09 13:29:43 +0430

    remove export button

--- src/widgets/subwidgets/button_widget.py
+++ src/widgets/subwidgets/button_widget.py
@@ -9,17 +9,13 @@ class ButtonWidget(QWidget):
     """
     Class for creating Buttons in the main window
     """
-    def __init__(self, exec_slot, export_slot):
+    def __init__(self, exec_slot):
         super().__init__()
 
         self.exec_button = QPushButton()
         self.exec_button.setText(EXEC_BUTTON_TEXT)
-        self.export_button = QPushButton()
-        self.export_button = QPushButton(EXPORT_BUTTON_TEXT)
 
         self.layout = QHBoxLayout()
         self.layout.addWidget(self.exec_button)
-        self.layout.addWidget(self.export_button)
 
         self.exec_button.clicked.connect(exec_slot)
-        self.export_button.clicked.connect(export_slot)
