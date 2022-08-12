b9c878b49e85e4d99cc48bd89578964ac01da96c
behrooz3500 <behrooz3500@yahoo.com>
2022-08-12 09:47:02 +0430

    using new window type

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import sys
 
 # internal
 from src.widgets import main_window as wd
+from src.widgets.subwidgets import tab_widget as tw
 
 # PyQt
 from PyQt5.QtWidgets import QApplication
@@ -11,7 +12,8 @@ from PyQt5.QtWidgets import QApplication
 def main():
 
     app = QApplication(sys.argv)
-    window = wd.MainWindow()
+    # window = wd.MainWindow()
+    window = tw.TabWidget()
     window.show()
     sys.exit(app.exec_())
 
