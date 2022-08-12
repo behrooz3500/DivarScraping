6450b6d5e0bd71ebdb457200b5183334da7ac0cb
behrooz3500 <behrooz3500@yahoo.com>
2022-08-12 15:14:04 +0430

    use constant variables

--- src/widgets/subwidgets/settings_tab.py
+++ src/widgets/subwidgets/settings_tab.py
@@ -7,6 +7,8 @@ from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout, QWidget, \
 
 # internal
 from src.constants import SettingsTabConstants as stc
+from src.constants import SettingsParameterConstants as spc
+from src.constants import GlobalConstants as gc
 
 
 class SettingTab(QWidget):
@@ -47,19 +49,19 @@ class SettingTab(QWidget):
         self.btn_layout.addWidget(self.save_btn)
         self.btn_layout.addWidget(self.default_btn)
 
-        with open("settings.json", "rt") as f:
+        with open(gc.SETTINGS_FILE_NAME, "rt") as f:
             setting = json.load(f)
 
-        if setting.get("Scroll mode") == "automatic":
+        if setting.get(spc.SCROLL_MODE) == spc.SCROLL_MODE_AUTO:
             self.automatic_radio_btn.setChecked(True)
             self.scroll_number.setDisabled(True)
-        elif setting.get("Scroll mode") == "manual":
+        elif setting.get(spc.SCROLL_MODE) == spc.SCROLL_MODE_MANUAL:
             self.manual_radio_btn.setChecked(True)
             self.scroll_number.setDisabled(False)
-        self.scroll_wait_time_edit.setText(setting.get("Scroll wait"))
-        self.time_out_edit.setText(setting.get("Time out"))
-        self.hide_image_checkbox.setChecked(setting.get("Hide images"))
-        self.windows_maximized_checkbox.setChecked(setting.get("Maximize window"))
+        self.scroll_wait_time_edit.setText(setting.get(spc.SCROLL_WAIT_TIME))
+        self.time_out_edit.setText(setting.get(spc.SCROLL_TIME_OUT))
+        self.hide_image_checkbox.setChecked(setting.get(spc.HIDE_IMAGE_SETTING))
+        self.windows_maximized_checkbox.setChecked(setting.get(spc.MAXIMIZE_PAGE_SETTING))
 
         self.main_layout.addLayout(self.line_one_layout)
         self.main_layout.addLayout(self.btn_layout)
