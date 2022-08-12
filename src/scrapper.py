233dec1fc215860ec9bd73924c1b8a9d57fcd2b4
behrooz3500 <behrooz3500@yahoo.com>
2022-08-12 15:12:46 +0430

    1-use constant variable 2-remove redundant functions 3-disable javascript in page 4-add possibility for zooming out the page in future

--- src/scrapper.py
+++ src/scrapper.py
@@ -11,10 +11,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
+from pyautogui import keyUp, keyDown, press
+
+# internal constants
+from src.constants import ScrapperConstants as sc
+from src.constants import SettingsParameterConstants as spc
+from src.constants import GlobalConstants as gc
 
 # internal
-from src.widgets import message_box as MB
-from src.constants import *
 from src import memory as mem
 from src import utils
 
@@ -27,12 +31,11 @@ class Scrapper:
     delay_time = delay time in seconds after each scroll to get the links\n
     pattern = pattern of the links to obtain\n
     """
-    def __init__(self, browser='Firefox'):
-        self.URL = mem.get("url")
-        self.scroll_count = mem.get("scroll_count")
-        self.delay_time = mem.get("delay_time")
-        self.pattern = mem.get("pattern")
-        self.time_out = mem.get("time_out")
+    def __init__(self, browser=sc.FIREFOX_BROWSER_NAME):
+        self.scroll_count = mem.get(spc.SCROLL_COUNT)
+        self.delay_time = mem.get(spc.SCROLL_WAIT_TIME)
+        self.pattern = mem.get(gc.PATTERN_TEXT)
+        self.time_out = mem.get(spc.SCROLL_TIME_OUT)
         self.browser = browser
         self.driver = None
         self.has_progress = True
@@ -43,10 +46,10 @@ class Scrapper:
 
     def initialize(self, url):
 
-        self.scroll_count = mem.get("scroll_count")
-        self.delay_time = mem.get("delay_time")
-        self.pattern = mem.get("pattern")
-        self.time_out = mem.get("time_out")
+        self.scroll_count = mem.get(spc.SCROLL_COUNT)
+        self.delay_time = mem.get(spc.SCROLL_WAIT_TIME)
+        self.pattern = mem.get(gc.PATTERN_TEXT)
+        self.time_out = mem.get(spc.SCROLL_TIME_OUT)
 
         def resource_path(relative_path):
             try:
@@ -55,28 +58,40 @@ class Scrapper:
                 base_path = os.path.dirname(__file__)
             return os.path.join(base_path, relative_path)
 
-        if self.browser == 'Firefox':
+        if self.browser == sc.FIREFOX_BROWSER_NAME:
             if win_arch() == "64":
-                file_name = FIREFOX_64_DRIVER_DIR
+                file_name = sc.FIREFOX_64_DRIVER_DIR
             else:
-                file_name = FIREFOX_32_DRIVER_DIR
+                file_name = sc.FIREFOX_32_DRIVER_DIR
             firefox_profile = webdriver.FirefoxProfile()
-            if mem.get("hide_images"):
+            if mem.get(spc.HIDE_IMAGE_SETTING):
                 # block image load to improve page loading speed
                 firefox_profile.set_preference('permissions.default.image', 2)
+                firefox_profile.set_preference(' javascript.enabled', False)
                 firefox_profile.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
 
             my_dir = resource_path(file_name)
             service = Service(executable_path=my_dir)
             self.driver = webdriver.Firefox(firefox_profile, service=service)
         else:
-            MB.MessageBox(IN_PROGRESS_MESSAGE).pop_up_box()
+            pass
 
         # open base url
         self.driver.get(url)
-        if mem.get("maximize_window"):
+        if mem.get(spc.MAXIMIZE_PAGE_SETTING):
             # maximizing windows
             self.driver.maximize_window()
+
+            # zooming out the page (not stable)
+            # if True:
+            #     for i in range (5):
+            #         keyDown('ctrl')
+            #         press('-')
+            #         keyUp('ctrl')
+            #     keyDown('ctrl')
+            #     press('r')
+            #     keyUp('ctrl')
+            #     time.sleep(3)
         self.count = 0
         self.has_progress = True
         self.links.clear()
@@ -95,17 +110,17 @@ class Scrapper:
         for a in anchors:
 
             href = a.get_attribute('href')
-            if href.startswith(mem.get("pattern")):
-                class_name = a.find_elements(By.CLASS_NAME, "kt-post-card__bottom-description")
+            if href.startswith(mem.get(gc.PATTERN_TEXT)):
+                class_name = a.find_elements(By.CLASS_NAME, sc.DIVAR_STORE_CLASS_NAME)
                 for cl in class_name:
                     attr = cl.get_attribute('title')
-                    if attr.find("پیش") == -1 and attr.find("روز") == -1:
+                    if attr.find(sc.DIVAR_ATT1) == -1 and attr.find(sc.DIVAR_ATT2) == -1:
                         self.store_name.append(attr)
                     if self.store_name.count(attr) < 2:
                         self.links.add(unquote(href))
 
         body.send_keys(Keys.PAGE_DOWN)
-        time.sleep(int(mem.get("delay_time")))
+        time.sleep(int(mem.get(spc.SCROLL_WAIT_TIME)))
         f_len = len(self.links)
 
         a = utils.file_name_edit(url)
@@ -119,7 +134,7 @@ class Scrapper:
         print(self.time_out)
         if self.count > int(self.time_out):
             self.has_progress = False
-        self.save_history()
+
         print(f"{self.count}>>after checks")
         self.scroll_count += 1
 
@@ -129,13 +144,6 @@ class Scrapper:
     def progress_check(self):
         return self.has_progress
 
-    def save_history(self):
-        try:
-            with open(HISTORY_TEXT_FILE_NAME, 'a', encoding="utf-8") as f:
-                f.write(f"{self.URL}\n\n")
-        except:
-            pass
-
 
 def export_links(generated_links, name):
     """Export results to a txt file"""
@@ -143,18 +151,6 @@ def export_links(generated_links, name):
     with open(f"{name}.txt", 'wt', encoding="utf-8") as f:
         for i, link in enumerate(generated_links, 1):
             f.write(f'{link}\n\n')
-
-
-def load_files():
-    try:
-        with open(OUTPUT_TEXT_FILE_NAME, 'r', encoding="utf-8") as f:
-            old_set = set()
-            for line in f:
-                strip_lines = line.strip()
-                old_set.add(strip_lines)
-        return old_set
-    except:
-        return set()
 
 
 def win_arch():
