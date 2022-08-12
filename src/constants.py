33fb3de487976c83569ad6e5a7bed24b8d53e4aa
behrooz3500 <behrooz3500@yahoo.com>
2022-08-12 15:11:14 +0430

    add new constants and remove redundant ones

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,14 @@
 class TabWidgetConstants:
-    pass
+    TAB_1_NAME = "main"
+    TAB_2_NAME = "Result"
+    TAB_3_NAME = "Settings"
+    RESUME_BUTTON_TEXT = "Resume"
+    PAUSE_BUTTON_TEXT = "Pause"
+    WINDOW_WIDTH = 600
+    WINDOW_HEIGHT = 400
+    WINDOWS_TITLE = "Web Scraping"
+    FILL_THE_BLANKS_ERROR = "Please fill in the blanks"
+    WRONG_INPUT = "Please enter valid inputs"
 
 
 class MainTabConstants:
@@ -36,48 +45,32 @@ class SettingsTabConstants:
     DEFAULT_BUTTON_TEXT = "Restore to defaults"
 
 
-# TabWidget Constants
-WINDOW_WIDTH = 600
-WINDOW_HEIGHT = 400
-WINDOWS_X = 250
-WINDOWS_Y = 250
-WINDOWS_TITLE = "Web Scraping"
-FILL_THE_BLANKS_ERROR = "Please fill in the blanks"
-OUTPUT_TEXT_FILE_NAME = "links.txt"
-HISTORY_TEXT_FILE_NAME = "history.txt"
-WRONG_INPUT = "Please enter valid inputs"
+class SettingsParameterConstants:
+    SCROLL_MODE = "scroll_mode"
+    SCROLL_COUNT = "number_of_scrolls"
+    SCROLL_MODE_AUTO = "automatic"
+    SCROLL_MODE_MANUAL = "manual"
+    SCROLL_WAIT_TIME = "scroll_wait_time"
+    SCROLL_TIME_OUT = "time_out"
+    HIDE_IMAGE_SETTING = "hide_image"
+    MAXIMIZE_PAGE_SETTING = "maximize_page"
 
-# Url_Widget Constants
-DEFAULT_URL = 'https://divar.ir/s/iran/home-kitchen?q=%D9%84%D9%88%D8%A7%D8%B2%D9%85%20%D8%AE%D8%A7%D9%86%DA%AF%DB%8C'
-URL_HINT_TEXT = "Enter URL please..."
-URL_BOX_WIDTH = 500
-CLEAR_HISTORY_BUTTON_TEXT = "Clear history"
 
-# Label_Widget Constants
-SCROLL_COUNT_LABEL_TEXT = "Number of Scrolls:"
-DELAY_LABEL_TEXT = "Delay in seconds:"
-PATTERN_BOX_DEFAULT = 'https://divar.ir/v'
-BROWSER_LIST = ["Firefox", "Chrome"]
-SCROLL_INPUT_WIDTH = 40
-DELAY_INPUT_WIDTH = 25
-PATTERN_INPUT_WIDTH = 150
-INPUT_HEIGHT = 20
+class GlobalConstants:
+    URLS_TEXT = "urls"
+    SETTINGS_FILE_NAME = "settings.json"
+    PATTERN_TEXT = "pattern"
 
-# Result_Widget Constants
-LINKS_BOX_WIDTH = 580
-LINKS_BOX_HEIGHT = 290
 
-# Button_Widget Constants
-EXEC_BUTTON_TEXT = "Execute"
-EXPORT_BUTTON_TEXT = "Export"
+class ScrapperConstants:
+    FIREFOX_BROWSER_NAME = "Firefox"
+    DIVAR_STORE_CLASS_NAME = "kt-post-card__bottom-description"
+    DIVAR_ATT1 = "پیش"
+    DIVAR_ATT2 = "روز"
+    FIREFOX_32_DRIVER_DIR = "./Drivers/geckodriver32.exe"
+    FIREFOX_64_DRIVER_DIR = "./Drivers/geckodriver64.exe"
+
 
 # MessageBox Constants
 BOX_TITLE = "Attention"
 CLEAR_HISTORY_TEXT = "Clear all histories?"
-
-# Scraper Constants
-IN_PROGRESS_MESSAGE = "Not available in current version"
-FIREFOX_32_DRIVER_DIR = "./Drivers/geckodriver32.exe"
-FIREFOX_64_DRIVER_DIR = "./Drivers/geckodriver64.exe"
-CHECK_NEW_LINKS = 5
-
